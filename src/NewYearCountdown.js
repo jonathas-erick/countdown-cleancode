8cef3e780f483242900d611d01d619e7684b371d
Jonathas <jonathas.erickl@gmail.com>
2023-02-13 09:48:39 -0300

    implementing features...

--- src/NewYearCountdown.js
+++ src/NewYearCountdown.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 function NewYearCountdown() {
+  const [targetDate, setTargetDate] = useState(
+    new Date("December 31, 2023 23:59:59")
+  );
   const [timeLeft, setTimeLeft] = useState(null);
 
   useEffect(() => {
-    const targetDate = new Date("December 31, 2023 23:59:59");
     const intervalId = setInterval(() => {
       const currentTime = new Date();
       const timeDifference = targetDate - currentTime;
@@ -12,13 +14,26 @@ function NewYearCountdown() {
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [targetDate]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const inputDate = new Date(event.target.elements.date.value);
+    setTargetDate(inputDate);
+  };
 
   return (
     <div>
       {timeLeft ? (
         <div>
-          <p className="headline">Time until New Year 2024!🚀</p>
+          <p className="headline">Time until the selected date!🚀</p>
+
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <input type="datetime-local" name="date" />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
 
           <div className="container">
             <p>
